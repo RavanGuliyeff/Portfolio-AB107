7f3f78f002236032a9581733fa2ddc3226232024
RavanGuliyeff <guliyevtr-ap116@code.edu.az>
2024-10-10 22:18:19 +0400

    ref-out

---
+++ Stack-Heap_Value-Reference_types_Ref-Out/insert-array.cs
@@ -0,0 +1,25 @@
+﻿//InsertArray methodu yaradılır.
+//Method parametr olaraq int tipindən array və params olaraq int-lər qəbul edir.
+//Paramslardan gələn dəyərləri həmin arraya əlavə etsin
+//və sonda dəyişilmiş hal köhnə arraya bərabər olsun. 
+//(Array resize istifade etmirsiz ozunuz Custom yazirsiz)
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr;
+            InsertArray(out arr, 1, 2, 3, 5, 2);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void InsertArray(out int[] arr, params int[] arr2)
+        {
+            arr = arr2;
+        }
+    }
+}
