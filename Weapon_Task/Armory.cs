5cf46e8961171fc9c6d7b3e9d0b7a4b21afc1d56
RavanGuliyeff <guliyevtr-ap116@code.edu.az>
2024-10-18 11:31:36 +0400

    Console app about Weapons

---
+++ Weapon_Task/Armory.cs
@@ -0,0 +1,43 @@
+﻿namespace Weapon_Task
+{
+    internal class Armory
+    {
+        public readonly string name = "My armory";
+
+        Weapon[] weapons = new Weapon[0];
+
+        public void AddWeapon(Weapon weapon)
+        {
+            Array.Resize(ref weapons, weapons.Length + 1);
+            weapons[^1] = weapon;
+        }
+
+        public void ShowWeapons()
+        {
+            foreach (Weapon weapon in weapons)
+            {
+                Console.WriteLine($"Silah adi: {weapon.Name}\nDaraq tutumu: {weapon.BulletCapacity}\nCari gulle sayi: {weapon.CurrentBullet}\nAtis modu: {weapon.FireMode}\nSilah ID: {weapon.ID}\n");
+            }
+        }
+
+        public Weapon GetWeapon(int id)
+        {
+            Weapon weapon = null;
+            do
+            {
+                foreach (Weapon w in weapons)
+                {
+                    if (id == w.ID)
+                    {
+                        weapon = w;
+                        return weapon;
+                    }
+                }
+                Console.WriteLine("Duzgun ID daxil edin");
+            }
+            while (weapon == null);
+            return weapon;
+        }
+
+    }
+}
